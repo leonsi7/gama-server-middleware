3cd7cec12f62c81c183fabbe246b34274e99d35f
leonsi7 <104212258+leonsi7@users.noreply.github.com>
2023-11-10 10:51:23 +0700

    Few changes

--- start.sh
+++ start.sh
@@ -1,25 +1,3 @@
-# #!/bin/bash
-
-# # Exécute npm start en arrière-plan et enregistre le PID dans un fichier
-# npm start & echo $! > server_pid.txt
-
-# # Attendez 0,5 seconde (500 millisecondes)
-# sleep 0.5
-
-# # Ouvre la page web dans Firefox
-# open "http://localhost:8000"
-
-# # Attend que l'utilisateur appuie sur "Entrée" pour quitter
-# read -p "Appuyez sur Entrée pour quitter..."
-
-# # Récupère le PID du serveur à partir du fichier et tue le processus
-# server_pid=$(cat server_pid.txt)
-# kill $server_pid
-
-#!/bin/bash
-
-#!/bin/bash
-
 npm install
 
 # Ensuite, ouvrez la page web dans Firefox
@@ -29,7 +7,7 @@ open "http://localhost:8000"
 npm start
 
 # Attend que l'utilisateur appuie sur "Entrée" pour quitter
-read -p "Appuyez sur Entrée pour quitter..."
+read -p "Press enter to quit..."
 
 
 
